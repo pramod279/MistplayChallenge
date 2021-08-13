5c11ac88605f1ac562eee80424b3515b30aae955
Pramod Selvaraj <pramod.selvaraj@experionglobal.com>
2021-08-13 12:32:12 +0530

    Code Optimisation

--- app/src/main/java/com/mistplay/challenge/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mistplay/challenge/ui/dashboard/DashboardFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.mistplay.challenge.R
 import com.mistplay.challenge.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
