aaf9f6a62f31308f1d867df1920d2ca14f01b84b
Pramod Selvaraj <pramod.selvaraj@experionglobal.com>
2021-08-14 23:08:24 +0530

    Json File Reader

--- app/src/main/java/com/mistplay/challenge/utils/Utils.kt
+++ app/src/main/java/com/mistplay/challenge/utils/Utils.kt
@@ -1,4 +1,20 @@
 package com.mistplay.challenge.utils
 
-class Utils {
+import android.content.Context
+import java.io.IOException
+
+object Utils {
+    /**
+     * Function for Fetching Json Data From Assets Folder
+     */
+    fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
 }
