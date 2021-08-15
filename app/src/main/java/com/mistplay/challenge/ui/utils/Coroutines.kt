a425de567cee5390540df9d7a327d65267069e4e
Pramod Selvaraj <pramod.selvaraj@experionglobal.com>
2021-08-15 22:20:43 +0530

    Integrated Coroutines

---
+++ app/src/main/java/com/mistplay/challenge/ui/utils/Coroutines.kt
@@ -0,0 +1,29 @@
+package com.mistplay.challenge.ui.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+/**
+ * Coroutines Helper Class
+ *
+ * Created by Pramod Selvaraj on 13/08/2021.
+ */
+object Coroutines {
+    /**
+     * This function is basically used for Calling function from IO Thread and
+     * return to the Main Thread
+     * ioToMainThread() is a Generic function and it will take 2 parameter
+     * A : work -> this is a suspended function
+     * B : callback -> which will take return data from
+     *                 work function and return the caller
+     */
+    fun <T : Any> ioThenMain(work: suspend (() -> T?), callback: (T?) -> Unit) =
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = CoroutineScope(Dispatchers.IO).async pramod@{
+                return@pramod work()
+            }.await()
+            callback(data)
+        }
+}
